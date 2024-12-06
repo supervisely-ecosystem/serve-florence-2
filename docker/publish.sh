8bf605495d640321c73a459c541fdae6d6e91bcb
GoldenAnpu <seryios.sych@gmail.com>
2024-12-06 12:00:14 +0100

    Bump Docker image version to 1.0.1 for Florence2

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,4 +1,4 @@
 cp ../dev_requirements.txt . && \
 cp ../src/models.json . && \
-docker build -t supervisely/florence-2:1.0.0 . && \
-docker push supervisely/florence-2:1.0.0
+docker build -t supervisely/florence-2:1.0.1 . && \
+docker push supervisely/florence-2:1.0.1
